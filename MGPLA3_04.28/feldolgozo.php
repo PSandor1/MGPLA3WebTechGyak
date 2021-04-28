da6cb8239f8fea6edc3e671a7638884b75b3535e
PSandor1 <palotas.s.89@gmail.com>
2021-04-28 12:03:23 +0200

    11.gyakorlat

---
+++ MGPLA3_04.28/feldolgozo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+  <meta charset="UTF-8">
+
+  <title>Feldolgozó PHP</title>
+ </head>
+ <body>
+  <font style="font-size:18pt">
+
+  <h2>HTML5 ûrlap </h2>
+
+  <?php
+
+
+
+echo ("<strong>Név:</strong>" . $_POST['nev'] . "<br><br>");
+
+echo ("<strong>PIN kód:</strong>" . $_POST['kod'] . "<br><br>");
+
+echo ("<strong>Gyümölcs:</strong>" . $_POST['gyumolcs'] . "<br><br>");
+
+echo ("<strong>Életkor</strong>" . $_POST['eletkor'] . "<br><br>");
+
+echo ("<strong>Lábméret:</strong>" . $_POST['labmeret'] . "<br><br>");
+
+echo ("<strong>Önbizalom:</strong>" . $_POST['onbizalom'] . "<br><br>");
+
+
+
+
+?>
+
+<a href="urlap3.html">vissza az ûrlapra . . . </a>
+ </body>
+</html>
